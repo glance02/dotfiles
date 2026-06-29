86aaa980d0b60037ce30df017ede23bd652e1338
glance <2431916178@qq.com>
2026-06-29 13:15:22 +0800

    添加tldr配置

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -93,6 +93,7 @@ Invoke-Expression (&starship init powershell)
 
 # 走clash流量
 $env:HTTP_PROXY="http://127.0.0.1:7897"; $env:HTTPS_PROXY="http://127.0.0.1:7897"
+$env:TLDR_LANGUAGE="zh"
 
 # 初始化base环境
 mamba activate base
