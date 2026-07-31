774dd0f3126cba5629739ec8d2d7c7fb931c2273
Sputnik <2431916178@qq.com>
2026-08-01 00:02:39 +0800

    停止追踪 PowerShell profile

--- Documents/WindowsPowerShell/profile.ps1
+++
@@ -1,7 +0,0 @@
-
-#region mamba initialize
-# !! Contents within this block are managed by 'mamba shell init' !!
-$Env:MAMBA_ROOT_PREFIX = "C:\Users\glance\miniforge3"
-$Env:MAMBA_EXE = "C:\Users\glance\miniforge3\Library\bin\mamba.exe"
-(& $Env:MAMBA_EXE 'shell' 'hook' -s 'powershell' -r $Env:MAMBA_ROOT_PREFIX) | Out-String | Invoke-Expression
-#endregion
